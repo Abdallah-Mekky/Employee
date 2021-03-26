778923e707f748ab25d10804b7f5fa4837898ed8
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2021-03-26 20:13:13 +0200

    Update EmployeeDaily.java

--- EmployeeDaily.java
+++ EmployeeDaily.java
@@ -1,22 +1,23 @@
 package employee.s.project;
 
 public class EmployeeDaily extends Employee {
-   
-    protected double Daily_Salary;
-    
-    public EmployeeDaily(String Na, String Add, double Da){
-    super(Na, Add);
-    this.Daily_Salary=Da;
+
+    private double Daily_Salary;
+
+    public EmployeeDaily(String Na, String Add, double Da) {
+        super(Na, Add);
+        this.Daily_Salary = Da;
     }
-    
-    public void setName(String Na){
-    this.name=Na;
+
+    public void setName(String Na) {
+        this.name = Na;
     }
 
     public void setDaily_Salary(double Daily) {
-        if(Daily>0){
-        this.Daily_Salary = Daily;}
-        
+        if (Daily > 0) {
+            this.Daily_Salary = Daily;
+        }
+
     }
 
     public void setAddress(String Add) {
@@ -34,20 +35,19 @@ public class EmployeeDaily extends Employee {
     public String getAddress() {
         return address;
     }
-    
-     public double yearlySala(double Daily_salary){
-    return Daily_salary*365;
+
+    public double yearlySala(double Salary) {
+        return Salary * 365;
     }
-     
-         public double Raise(double Daily_salary){
-Daily_salary = Daily_salary +(Daily_salary*0.05);
-    return yearlySala(Daily_salary);
+
+    public void Raise(double Salary) {
+        double raise = (Salary * 0.05) + yearlySala(Salary);
+        System.out.println("Your Yearly-Salary After Raise = " + raise);
     }
 
     @Override
     public String toString() {
-        return super.toString()+  "Daily_Salary Is " + Daily_Salary + '}';
+        return super.toString() + "Daily_Salary Is " + Daily_Salary + '}';
     }
-    
-      
+
 }
